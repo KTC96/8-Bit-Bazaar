a7da853147fa743c9fb7ecd4fab48fb9f50c2c0a
KTC96 <kyleclow96@gmail.com>
2024-02-01 18:08:36 +0000

    Add game cards and display on games.html and style

--- games/views.py
+++ games/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Game
 
-# Create your views here.
+def all_games(request):
+    """A view to show all games, with sorting and searching"""
+
+    games = Game.objects.all()
+
+    context = {
+        'games': games,
+    }
+
+    return render(request, 'games/games.html', context)
+
+    
+
