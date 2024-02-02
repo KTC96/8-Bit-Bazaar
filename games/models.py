c9cb5178a4a28be8897c82a1b41fa479a71ccbd8
KTC96 <kyleclow96@gmail.com>
2024-02-02 13:44:56 +0000

    Add game detail html, view and url

--- games/models.py
+++ games/models.py
@@ -63,6 +63,7 @@ class Game(models.Model):
     platform = models.ForeignKey('Platform', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
@@ -73,6 +74,10 @@ class Game(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 
 
--- games/views.py
+++ games/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 def all_games(request):
@@ -12,5 +12,15 @@ def all_games(request):
 
     return render(request, 'games/games.html', context)
 
-    
 
+
+def game_detail(request, game_id):
+    """A view to show individual game details"""
+
+    game = get_object_or_404(Game, pk=game_id)
+
+    context = {
+        'game': game,
+    }
+
+    return render(request, 'games/game_detail.html', context)
