c6b3e6dd83e4a789c8a8f5e22e1dbc9086b97404
KTC96 <kyleclow96@gmail.com>
2024-02-13 17:19:59 +0000

    Add checkout success logic and fixed issue with stripe payments

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,30 @@
-import json
-
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
+from .forms import OrderForm
 from .models import Order, OrderLineItem
 from games.models import Game
 from bag.contexts import bag_contents
-from .forms import OrderForm
 
 import stripe
 
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -31,91 +45,76 @@ def checkout(request):
             'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save(commit=False)
-            pid = request.POST.get('client_secret').split('_secret')[0]
-            order.stripe_pid = pid
-            order.original_bag = json.dumps(bag)
-            order.save()
+            order = order_form.save()
             for item_id, item_data in bag.items():
                 try:
                     game = Game.objects.get(id=item_id)
-                    quantity = item_data
-                    order_line_item = OrderLineItem(
-                        order=order,
-                        game=game,
-                        quantity=item_data,
-                    )
-                    order_line_item.save()
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            game=game,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
 
                 except Game.DoesNotExist:
-                    messages.error(request, (
-                        "We couldn't locate one of the games from your bag in our database."
-                        " Please reach out to us for help!")
-                    )
-                    order.delete()
+                    messages.error(request, f"The game with ID {item_id} in your bag wasn't found in our database.")
+                    # Optionally, you can choose to skip this item and continue processing other items
+                    continue
 
-            request.session ['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number] ))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(request, 'Oops! There was an error with your form. \
-                Please double check your information.')
-
-
+            messages.error(request, 'There was an error with your form. Please double-check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "Your bag currently lacks any old-school games.")
+            messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse('games'))
 
         current_bag = bag_contents(request)
         total = current_bag['total']
-        stripe_total = round(total *100)
+        stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
 
-        if not stripe_public_key:
-            messages.warning(request, 'Stripe public key is missing. \
-                Did you forget to set it in your environment?')
-
-        print(intent)
         order_form = OrderForm()
-        template = 'checkout/checkout.html'
-        context = {
-            'order_form': order_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': intent.client_secret,
-        }
 
-        return render(request, template, context)
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
 
 
 def checkout_success(request, order_number):
     """
-    Handle view after a successful checkout
+    Handle successful checkouts
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    
-    messages.success(request, f'Order successfuly processed!\
-        Your order number is {order_number}. A confirmation email \
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
 
     template = 'checkout/checkout_success.html'
-
     context = {
-        'order': order
+        'order': order,
     }
 
     return render(request, template, context)
-
-
-
