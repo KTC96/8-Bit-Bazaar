f5e4e9787e161c59d0fe308c30792a422d1140b2
KTC96 <kyleclow96@gmail.com>
2024-02-14 13:37:17 +0000

    Complete webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -39,30 +39,16 @@ class StripeWH_Handler:
             intent.latest_charge
         )
 
-        billing_details = stripe_charge.billing_details # updated
+        billing_details = stripe_charge.billing_details 
         
-        total = round(stripe_charge.amount / 100, 2) # updated
+        total = round(stripe_charge.amount / 100, 2)
 
         # Clean data in the billing details
         for field, value in billing_details.address.items():
             if value == "":
                 billing_details.address[field] = None
 
-        # Update profile information if save_info was checked
-        profile = None
-        username = intent.metadata.username
-        if username != 'AnonymousUser':
-            profile = UserProfile.objects.get(user__username=username)
-            if save_info:
-                profile.default_phone_number = billing_details.phone
-                profile.default_country = billing_details.address.country
-                profile.default_postcode = billing_details.address.postal_code
-                profile.default_town_or_city = billing_details.address.city
-                profile.default_street_address1 = billing_details.address.line1
-                profile.default_street_address2 = billing_details.address.line2
-                profile.default_county = billing_details.address.state
-                profile.save()
-
+    
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -95,7 +81,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=billing_details.name,
-                    user_profile=profile,
+                    # user_profile=profile,
                     email=billing_details.email,
                     phone_number=billing_details.phone,
                     country=billing_details.address.country,
