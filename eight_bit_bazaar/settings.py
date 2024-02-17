450ff2d284608b2499c3ab1784333ca0a4d8fd01
KTC96 <kyleclow96@gmail.com>
2024-02-17 12:07:15 +0000

    Remove secret key and set debug

--- eight_bit_bazaar/settings.py
+++ eight_bit_bazaar/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)c+z6vhhp(-mgpa4l)l1f(k2lqo968h$q!6*w#vh%d=r$6^!q'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-ktc96-8bitbazaar-2p4d88y7kk8.ws-eu108.gitpod.io', 'eight-bit-bazaar-8c5cb6f7cbb6.herokuapp.com']
 
