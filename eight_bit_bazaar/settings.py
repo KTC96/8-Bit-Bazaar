b9e39fd471aca5c0c510f322998f7ff64d18630b
KTC96 <kyleclow96@gmail.com>
2024-03-11 18:13:10 +0000

    Add aall html testing screenshots

--- eight_bit_bazaar/settings.py
+++ eight_bit_bazaar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['8000-ktc96-8bitbazaar-2p4d88y7kk8.ws-eu108.gitpod.io', 'eight-bit-bazaar-8c5cb6f7cbb6.herokuapp.com']
 
 
